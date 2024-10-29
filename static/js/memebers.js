e4507a7229dcb2112b7b5397e05d0dacb137b44c
Jaeyoung <jyko1101@gmail.com>
2024-10-29 12:17:48 +0900

    add modal for popup
    팝업을 위해 modal 기능을 빠르게 프로토타이핑해서 적용

--- static/js/memebers.js
+++ static/js/memebers.js
@@ -1,3 +1,17 @@
 
 console.log("test log: i am memberjs");
 
+function openModal(name, text, image) {
+    console.log("func :: openModal called");
+    // 모달에 데이터 설정
+    document.getElementById('modalName').textContent = name;
+    document.getElementById('modalText').textContent = text;
+    document.getElementById('modalImage').src = image;
+    
+    // 모달 띄우기
+    var myModal = new bootstrap.Modal(document.getElementById('memberModal'));
+    myModal.show();
+};
+
+// 전역으로 함수 노출
+window.openModal = openModal;
