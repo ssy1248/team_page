2debd303ec1c41e7424b94ba935b35e80f58e316
Jaeyoung <jyko1101@gmail.com>
2024-10-30 17:15:25 +0900

    feature: admin-create added

--- static/js/members_admin.js
+++ static/js/members_admin.js
@@ -7,7 +7,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { collection, query, where } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { doc, getDocs, updateDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { doc, addDoc, getDocs, updateDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -38,7 +38,7 @@ $(document).on('click', '.button-admin', function () {
     else
         $(this).css('filter', 'brightness(0.8)'); // 활성화 시 밝게
 
-        toggleAdminSpace();
+    toggleAdminSpace();
 });
 // 어드민 키 입력창 띄우기
 function toggleAdminSpace() {
@@ -51,15 +51,16 @@ $(document).on('click', '.button-admin-key-enter', async function () {
     console.log("[btn check] button-admin-key-enter pressed");
     let input = $('.admin-access-input').val();
     let isKeyMatched = await checkAdminKey(input);
-    if (isKeyMatched){
+    if (isKeyMatched) {
         showToast("correct");
     }
-    else{
+    else {
         showToast("wrong");
     }
+    $('.admin-access-input').val('');
 });
 // 토스트 메시지 띄우기
-function showToast(message) {
+function showToast(message, flag) {
     console.log("[func check] showToast called");
     $('.admin-toast').text(message); // 메시지 설정
     $('.admin-toast').addClass('showToast'); // 토스트 메시지 보이기
@@ -67,11 +68,11 @@ function showToast(message) {
     // 일정 시간 후 토스트 메시지 숨기기
     setTimeout(() => {
         $('.admin-toast').removeClass('showToast');
-    }, 3000); // 3초 후 사라짐
+    }, 1500); // 3초 후 사라짐
 };
 
 // firebase에서 어드민 키 확인하기
-async function checkAdminKey(input){
+async function checkAdminKey(input) {
     console.log("[func check] checkAdminKey called");
     try {
         const adminKeys = adminDocs.docs.map(admin => admin.data().key);
@@ -88,10 +89,121 @@ async function checkAdminKey(input){
 // admin 모드 상태 플래그
 let isAdmin = false;
 // admin 모드 진입
-function enterAdminMode(){
+function enterAdminMode() {
     isAdmin = true;
-}
+};
 // admin 모드 탈출
-function exitAdminMode(){
+function exitAdminMode() {
     isAdmin = false;
+};
+
+// ======================================================================================================================
+// ======================================================================================================================
+
+// 어드민 기능 : 멤버카드 생성 버튼
+$(document).on('click', '.button-admin-create', async function () {
+    console.log("[btn check] button-admin-create pressed");
+    createMemberCard();
+});
+// 멤버카드 생성 팝업 띄우기
+function createMemberCard() {
+    let myModal = new bootstrap.Modal($('#memberModal')[0]);
+    myModal.show();
+    let modalBody = $('.modal-body');
+    modalBody.empty();
+    let tmp = `
+                <h4 id="member-name"><input type="text" placehoder="팀원 이름" class="edited-name"></h4>
+                <button type="button" class="button-enroll button-basic"> <img src="../static/img/icon-enroll.png" alt="FAILED TO LOAD IMG" class="img-button-enroll">등록</button>
+                <img id="member-image" src="" alt="FAILED TO LOAD IMG" class="img-fluid mb-3"><input type="text" placehoder="이미지 URL" class="edited-image">
+                <p id="member-text">
+                    MBTI : <input type="text" placehoder="MBTI" class="edited-mbti">
+                    <br>
+                    자기소개 : <input type="text" placehoder="자기소개" class="edited-intro">
+                    <br>
+                    관심사 : <input type="text" placehoder="관심사" class="edited-interest">
+                    <br>
+                    좋아하는 게임 : <input type="text" placehoder="좋아하는 게임" class="edited-favGame">
+                    <br><br>
+                    + G A M E 키워드로 표현하기 +
+                    <br>
+                    G : <textarea rows="4" cols="30" class="edited-game_G" placehoder="G로 시작하는 키워드 영단어"></textarea>
+                    <br>
+                    A : <textarea rows="4" cols="30" class="edited-game_A" placehoder="G로 시작하는 키워드 영단어"></textarea>
+                    <br>
+                    M : <textarea rows="4" cols="30" class="edited-game_M" placehoder="G로 시작하는 키워드 영단어"></textarea>
+                    <br>
+                    E : <textarea rows="4" cols="30" class="edited-game_E" placehoder="G로 시작하는 키워드 영단어"></textarea>
+                    <br><br>
+                    Github Link : <input type="text" placehoder="Github Link" class="edited-linkGithub">
+                    <br>
+                    Blog Link :   <input type="text" placehoder="블로그 Link" class="edited-linkBlog">
+                </p>
+                <!-- GitHub, Blog 아이콘과 링크 -->
+                <div class="modal-icons">
+                </div>
+    `;
+    modalBody.append(tmp);
+};
+
+// 멤버카드 생성 팝업 모달에서 등록 버튼
+$(document).on('click', '.button-enroll', async function () {
+    console.log("[btn check] button-enroll pressed");
+    enrollMemberInfos();
+});
+
+// 등록버튼: 새로 입력한 멤버정보 입력
+async function enrollMemberInfos() {
+    console.log("[func check] enrollMemberInfos called");
+    try {
+        let newID = generateRandomID();
+        let newMember = {
+            'id': newID,
+            'name': $('.edited-name').val(),
+            'image': $('.edited-image').val(),
+            'mbti': $('.edited-mbti').val(),
+            'intro': $('.edited-intro').val(),
+            'interest': $('.edited-interest').val(),
+            'favGame': $('.edited-favGame').val(),
+            'game_G': $('.edited-game_G').val(),
+            'game_A': $('.edited-game_A').val(),
+            'game_M': $('.edited-game_M').val(),
+            'game_E': $('.edited-game_E').val(),
+            'linkGithub': $('.edited-linkGithub').val(),
+            'linkBlog': $('.edited-linkBlog').val()
+        };
+        // 새 멤버 데이터 추가
+        await addDoc(collection(db, "members"), newMember);
+        alert('새 팀원 등록완료');
+        window.location.reload();
+    } catch (e) {
+        console.error("[Error] cannot enroll member", e);
+    }
+};
+
+// ======================================================================================================================
+// ======================================================================================================================
+// id 생성 위한 난수 코드 생성
+
+function generateRandomID(){
+    const pool = '0123456789abcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    const len = 8;  // 8자리로 생성
+    for (let i = 0; i < len; i++) {
+        const randIndex = Math.floor(Math.random() * pool.length);
+        result += pool[randIndex];
+    }
+    return result;
 }
+
+// ======================================================================================================================
+// ======================================================================================================================
+
+// 어드민 기능 : 멤버카드 삭제
+$(document).on('click', '.button-admin-delete', async function () {
+    console.log("[btn check] button-admin-create pressed");
+    deleteMemberCard();
+});
+
+function deleteMemberCard() {
+
+};
